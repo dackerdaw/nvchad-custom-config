2372b2631068b6a7126b6b83877e7357f8d04f81
ibrahim-qordhawi <ibrahim.qordhawi@idstar.co.id>
2023-02-18 21:49:42 +0700

    comment out the line

--- init.lua
+++ init.lua
@@ -1,7 +1,7 @@
 local autocmd = vim.api.nvim_create_autocmd
 local opt = vim.opt
 
-Auto resize panes when resizing nvim window
+-- Auto resize panes when resizing nvim window
 autocmd("VimResized", {
   pattern = "*",
   command = "tabdo wincmd =",
