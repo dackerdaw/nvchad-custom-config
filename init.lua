e718e09f23445a0a1aeb0eeee921d59ef26a919d
dackerdaw <dacker.daw@gmail.com>
2023-02-18 18:49:09 +0700

    add auto open nvim-tree feature failed

--- init.lua
+++ init.lua
@@ -1,10 +1,15 @@
 local autocmd = vim.api.nvim_create_autocmd
 local opt = vim.opt
 
--- Auto resize panes when resizing nvim window
+Auto resize panes when resizing nvim window
 autocmd("VimResized", {
   pattern = "*",
   command = "tabdo wincmd =",
 })
+
+-- local function open_nvim_tree()
+--   require("nvim-tree.api").tree.open()
+-- end
+-- autocmd({ "VimEnter" }, { callback = open_nvim_tree })
 
 opt.relativenumber = true
